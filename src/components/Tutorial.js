146b33400b6c3dce8d67e5418a0cc5080024835c
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-06-30 01:44:02 -0300

    Add background color and text alignment to h1

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -21,7 +21,7 @@ function Tutorial({ nextStep }) {
       <img src={`${process.env.PUBLIC_URL}/images/amizades.jpg`} alt="Imagem tutorial" className="tutorial-image" />
       <h1>Tutorial de como ser um bom amigo com o Elias Junio Souza Ribeiro vulgamente conhecido como filho do Revetria/Pé Vermelho.</h1>
       <MusicPlayer isPlaying={isPlaying} />
-      {!isPlaying && <button className="button-play" onClick={handlePlay}>Clica nesse botão aqui para sentir remorso de não ter me convidado.</button>}
+      {!isPlaying && <button className="button-play" onClick={handlePlay}>Clique nesse botão para ser consumido pelo remorso de não ter me convidado e nem sequer ter lembrado de mim.</button>}
       {isPlaying && <button className="button" onClick={nextStep}>Próximo</button>}
     </div>
   );

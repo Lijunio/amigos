8c68d26e395d25b437ddf0800f5c449ee12ffcd7
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-06-30 01:26:20 -0300

    Fix play button positioning and visibility

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function MusicPlayer({ onPlay }) {
   const audioRef = useRef(null);

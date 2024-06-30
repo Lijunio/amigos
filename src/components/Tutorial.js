dda42a648166f235bdff4de72443eec69541e054
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-06-30 01:27:28 -0300

    Fix play button positioning and visibility

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -11,7 +11,7 @@ function Tutorial({ nextStep }) {
   return (
     <div className="container">
       <img src={`${process.env.PUBLIC_URL}/images/amizades.jpg`} alt="Imagem tutorial" className="tutorial-image" />
-      <h1>Tutorial de como não ser um bom amigo com meus amigos/familia do Alto Vera Cruz</h1>
+      <h1>Tutorial de como não ser um bom amigo com o Elias Junio Souza Ribeiro vulgamente conhecido como filho do Revetria</h1>
       <MusicPlayer onPlay={handlePlay} />
       {isPlaying && <button className="button" onClick={nextStep}>Próximo</button>}
     </div>
